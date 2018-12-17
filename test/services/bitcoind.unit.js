582c20a03a9596587fc84f7f6bb01f90c64ebfaf
Gametoshi <44985491+Globaleliteee@users.noreply.github.com>
2018-12-18 00:37:02 +0100

    change rpcports for gamecredits

--- test/services/bitcoind.unit.js
+++ test/services/bitcoind.unit.js
@@ -769,7 +769,7 @@ describe('Bitcoin Service', function() {
         }
       };
       var bitcoind = new BitcoinService(config);
-      bitcoind._getDefaultConf().rpcport.should.equal(9332);
+      bitcoind._getDefaultConf().rpcport.should.equal(40001);
     });
     it('will get default rpc port for testnet', function() {
       var config = {
